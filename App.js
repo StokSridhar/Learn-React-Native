7d85726f5fbf544371755a5fe1bbe0a09e2ac642
Sridhar <ronsridhar607@gmail.com>
2020-07-10 12:28:47 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,105 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {Header} from './Header';
-import {heading} from './Typography';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-const WelcomeScreen = () =>
-  <View>
-    <Header title="Welcome to React Native"/>
-    <Text style={heading}>Step One</Text>
-    <Text>
-      Edit App.js to change this screen and turn it
-      into your app.
-    </Text>
-    <Text style={heading}>See Your Changes</Text>
-    <Text>
-      Press Cmd + R inside the simulator to reload
-      your app’s code.
-    </Text>
-    <Text style={heading}>Debug</Text>
-    <Text>
-      Press Cmd + M or Shake your device to open the
-      React Native Debug Menu.
-    </Text>
-    <Text style={heading}>Learn</Text>
-    <Text>
-      Read the docs to discover what to do next:
-    </Text>
-   </View>
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button, Alert, Image
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.header}>Periyava Puranam</Text> 
+      </View>
+      <View style={styles.slider}>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://mahaperiyava.org/beta/wp-content/uploads/2019/09/Mainslide-1280x720.jpg',
+        }}
+      />
+      </View>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 175, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 200, height: 50, backgroundColor: 'skyblue'}} />
+         
+      </View> 
+      
+        
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  tinyLogo: {
+    width: 380,
+    height: 150,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  header: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 20,
+    paddingRight: 12,
+    textAlign: 'left',
+    backgroundColor: '#c03145', 
+    color: 'white', 
+  },
+  slider: {
+    backgroundColor:'#dcdcdc',
+    height:150,
+  }
+});
+
+export default App;
